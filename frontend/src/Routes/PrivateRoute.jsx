d034a35c62a679164e11fefdce1974d9ce293816
Hemant142 <yhemant203@gmail.com>
2025-05-10 09:19:49 +0530

    GoogleAuth

--- frontend/src/Routes/PrivateRoute.jsx
+++ frontend/src/Routes/PrivateRoute.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-    import Cookies from 'js-cookie';
-import Login from '../Pages/Login';
-const PrivateRoute = ({children}) => {
-   const token = Cookies.get("login_token");
-   return !token?<Login />:children
-}
+import Cookies from "js-cookie";
+import Login from "../Pages/Login";
+const PrivateRoute = ({ children }) => {
+  const token = Cookies.get("login_token");
+  return !token ? <Login /> : children;
+};
 
-export default PrivateRoute
+export default PrivateRoute;
